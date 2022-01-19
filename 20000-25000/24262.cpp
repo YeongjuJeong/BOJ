1c413ac6e258b613e609b13eb6dac32ea7090aa3
Yeongju Jeong <yeongjujeong1021@gmail.com>
2022-01-19 23:13:38 +0900

    Create 24262.cpp

---
+++ 20000-25000/24262.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+	cout << 1 << '\n' << 0 << '\n';
+
+	return 0;
+}
