17e9f1e610bd0728b6c964b20cd0e38c6ddce6ea
Yeongju Jeong <yeongjujeong1021@gmail.com>
2022-01-20 11:15:02 +0900

    Create 9316.cpp

---
+++ 5000-10000/9316.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n; cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+
+	return 0;
+}
