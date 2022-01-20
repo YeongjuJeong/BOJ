4d4ccebbb53890441cb3794226ddc96b3075d8ec
Yeongju Jeong <yeongjujeong1021@gmail.com>
2022-01-20 11:14:29 +0900

    Create 2163.cpp

---
+++ 1000-5000/2163.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	cout << n * m - 1 << '\n';
+
+	return 0;
+}
