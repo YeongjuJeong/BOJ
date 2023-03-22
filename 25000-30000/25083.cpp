ae60f1c7133a28b25ef80a838ea40c39db601566
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-22 21:07:29 +1100

    Create 25083.cpp

---
+++ 25000-30000/25083.cpp
@@ -0,0 +1,15 @@
+// 25083 새싹
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
+
+    return 0;
+}
