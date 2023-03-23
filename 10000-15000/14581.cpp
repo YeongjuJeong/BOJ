f1da84a38ea43a7ca23bac2c19be3cee9bbab8aa
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-23 23:02:41 +1100

    Create 14581.cpp

---
+++ 10000-15000/14581.cpp
@@ -0,0 +1,16 @@
+// 14581 팬들에게 둘러싸인 홍준
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << s << "::fan:\n";
+    cout << ":fan::fan::fan:";
+
+    return 0;
+}
