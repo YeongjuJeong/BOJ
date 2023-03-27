f178bcd17b24dd9e52b3da1d2c30027f87e7516c
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-27 17:04:05 +1100

    Create 14337.cpp

---
+++ 10000-15000/14337.cpp
@@ -0,0 +1,15 @@
+// 14337 Helicopter
+
+Module Main
+ 
+  Sub Main()
+    Console.WriteLine("      ===================")
+    Console.WriteLine("          ____||___")
+    Console.WriteLine("\ /      /       []\")
+    Console.WriteLine(" X=======           \__")
+    Console.WriteLine("/ \      \____________|")
+    Console.WriteLine("            ||  ||")
+    Console.WriteLine("         \-----------/")
+  End Sub
+ 
+End Module
