1341ed2118ac0a7a0f53483ef3fcbba5f329cfc0
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-27 16:51:03 +1100

    Create 10189.cpp

---
+++ 10000-15000/10189.cpp
@@ -0,0 +1,18 @@
+// 10189 Hook
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    
+    cout << "#  # #### #### #  #\n";
+    cout << "#### #  # #  # # #\n";
+    cout << "#### #  # #  # # #\n";
+    cout << "#  # #### #### #  #\n";
+    
+    return 0;
+}
