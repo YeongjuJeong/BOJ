11c6ca5944b4070a7c8103e243dc413ac3a71308
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-29 21:38:30 +1100

    Create 26082.cpp

---
+++ 25000-30000/26082.cpp
@@ -0,0 +1,16 @@
+// 26082 WARBOY
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    cout << b / a * 3 * c;
+
+    return 0;
+}
