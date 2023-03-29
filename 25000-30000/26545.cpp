49cc3226370dc8cb4f4f4c54bee2fbd5935585b2
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-29 22:16:53 +1100

    Create 26545.cpp

---
+++ 25000-30000/26545.cpp
@@ -0,0 +1,15 @@
+// 26545 Mathematics
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x, sum = 0;
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        cin >> x;
+        sum += x;
+    }
+    cout<<sum;
+    return 0;
+}
