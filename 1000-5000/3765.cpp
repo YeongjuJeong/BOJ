8f7f1cc3ca88cbafc699f88982cef2f26653313a
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-30 21:26:28 +1100

    Create 3765.cpp

---
+++ 1000-5000/3765.cpp
@@ -0,0 +1,13 @@
+// 3765 Celebrity jeopardy
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    string s;
+    while (getline(cin, s)){cout << s << '\n';}
+    return 0;
+}
