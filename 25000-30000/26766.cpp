0d4d55483c096b6d6607c07b991b44c3dd170a09
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-30 18:35:04 +1100

    Create 26766.cpp

---
+++ 25000-30000/26766.cpp
@@ -0,0 +1,21 @@
+// 26766 Serca
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    
+    for (int i = 0; i < n; i++){
+        printf(" @@@   @@@ \n");
+        printf("@   @ @   @\n");
+        printf("@    @    @\n");
+        printf("@         @\n");
+        printf(" @       @ \n");
+        printf("  @     @  \n");
+        printf("   @   @   \n");
+        printf("    @ @    \n");
+        printf("     @     \n");
+    }
+    return 0;
+}
