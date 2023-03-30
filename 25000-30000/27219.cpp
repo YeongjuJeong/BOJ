0d27d5a17a9665f74b4e2a11627eba668236d969
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-30 18:38:42 +1100

    Create 27219.cpp

---
+++ 25000-30000/27219.cpp
@@ -0,0 +1,10 @@
+// 27219 Робинзон Крузо
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    for (int i = 0; i < n / 5; i++) cout << "V";
+    for (int i = 0; i < n % 5; i++) cout << "I";
+    return 0;
+}
