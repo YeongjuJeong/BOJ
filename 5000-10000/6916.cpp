8b042588d5b6c56f9dbe48cee9191b8d1b58558c
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-03-31 20:08:36 +1100

    Create 6916.cpp

---
+++ 5000-10000/6916.cpp
@@ -0,0 +1,122 @@
+// 6916 0123456789
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n; cin >> n;
+    if (n == 0){
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *";
+    }
+    else if (n == 1){
+        cout << "\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+    }
+    else if (n == 2){
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+    }
+    else if (n == 3){
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    else if (n == 4){
+        cout << "\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+    }
+    else if (n == 5){
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    else if (n == 6){
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if (n == 7){
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+    }
+    else if (n == 8){
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if (n == 9){
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    return 0;
+}
