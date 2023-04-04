db6239971f456bfc008ad5b7cc74c61acba0e15d
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-04-05 01:08:52 +1000

    Create 15232.cpp

---
+++ 15000-20000/15232.cpp
@@ -0,0 +1,16 @@
+// 15232 Rectangles
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+    for (int i = 0; i < a; i++){
+        for (int j = 0; j < b; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
