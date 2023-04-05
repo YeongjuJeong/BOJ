436c6beca432ca5197d625ff3283f3d62fb457c1
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-04-06 01:02:00 +1000

    Create 15610.cpp

---
+++ 15000-20000/15610.cpp
@@ -0,0 +1,13 @@
+// 15610
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double n; cin >> n;
+    cout << fixed << setprecision(6) << sqrt(n) * 4;
+
+    return 0;
+}
