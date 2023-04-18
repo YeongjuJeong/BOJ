1648654938d3bd8309b1979a345dc8f95143a5e2
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-04-19 01:21:55 +1000

    Create 15372.cpp

---
+++ 15000-20000/15372.cpp
@@ -0,0 +1,15 @@
+// 15372 A Simple Problem.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    long long t, n; cin >> t;
+    while (t--){
+        cin >> n;
+        cout << n * n << '\n';
+    }
+    return 0;
+}
