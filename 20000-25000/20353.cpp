07faf106003c9c854959d287eb8cf26bb6c4174b
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-04-29 01:53:29 +1000

    Create 20353.cpp

---
+++ 20000-25000/20353.cpp
@@ -0,0 +1,14 @@
+// 20353 Atrium
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    long double a;
+    cin >> a;
+    cout << fixed << setprecision(6) << sqrt(a) * 4;
+    
+    return 0;
+}
