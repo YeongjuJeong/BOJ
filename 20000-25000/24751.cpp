bb8e165e3e52f7fd7ad51f98f2b698b74e7239a8
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-05-16 01:18:52 +1000

    Create 24751.cpp

---
+++ 20000-25000/24751.cpp
@@ -0,0 +1,13 @@
+// 24751 Betting
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double a; cin >> a;
+    cout << fixed << setprecision(6) << 100 / a << '\n' << 100 / (100 - a);
+    
+
+    return 0;
+}
