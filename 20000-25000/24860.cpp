6adc5212a8a54eb94af5d0042456edddb0ee3d1c
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-05-17 02:22:52 +1000

    Create 24860.cpp

---
+++ 20000-25000/24860.cpp
@@ -0,0 +1,12 @@
+// 24860 Counting Antibodies
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a, b, c, d, e, f, g;
+    cin >> a >> b >> c >> d >> e >> f >> g;
+    cout << (a * b + c * d) * e * f * g;
+
+    return 0;
+}
