b82bdeef87768e4d699789a0613e3aea03586685
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-05-18 01:31:45 +1000

    Create 24883.cpp

---
+++ 20000-25000/24883.cpp
@@ -0,0 +1,12 @@
+// 24883 자동완성
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c; cin >> c;
+    if (c == 'N' || c == 'n') cout << "Naver D2";
+    else cout << "Naver Whale";
+
+    return 0;
+}
