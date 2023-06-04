820d1bc7bf6bbfeff423a354b3d1207ae519b9b4
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-06-05 01:28:49 +1000

    Create 26057.cpp

---
+++ 25000-30000/26057.cpp
@@ -0,0 +1,13 @@
+// 26057 Большой удой
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int a, b; cin >> a >> b;
+    cout << b * 2 - a;
+    
+    return 0;
+}
