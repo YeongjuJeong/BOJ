f86e64d05177cef79b3f4092d2824cb76c7f60fe
Yeongju Jeong <yeongjujeong1021@gmail.com>
2023-09-11 23:47:05 +1000

    Create 28444.cpp

---
+++ 25000-30000/28444.cpp
@@ -0,0 +1,13 @@
+// 28444 HI-ARC=?
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h, i, a, r, c;
+    cin >> h >> i >> a >> r >> c;
+    cout << h * i - a * r * c;
+
+    return 0;
+}
